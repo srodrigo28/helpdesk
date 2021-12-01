efc27651bd48ac40da54f24ffd2d027922cf5517
srodrigo28 <rodrigoexer1@gmail.com>
2021-12-01 20:51:09 -0300

    tipos classes de dominios criados

---
+++ src/main/java/com/helpdesk/domain/Chamado.java
@@ -0,0 +1,104 @@
+package com.helpdesk.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import com.helpdesk.domain.enus.Prioridade;
+import com.helpdesk.domain.enus.Status;
+
+public class Chamado {
+	
+	private Integer id;
+	@SuppressWarnings("unused")
+	private LocalDate dataAbertura = LocalDate.now();
+	@SuppressWarnings("unused")
+	private LocalDate dataFechamento;
+	private Prioridade prioridade;
+	private Status status;
+	private String titulo;
+	private String observacoes;
+	
+	private Tecnico tecnico;
+	private Cliente cliente;
+	
+	public Chamado() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Prioridade getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chamado other = (Chamado) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
